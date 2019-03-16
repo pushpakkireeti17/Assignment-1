a78de233dcdfed01fea6100ca91754679811afa9
pushpakkireeti17 <36259555+pushpakkireeti17@users.noreply.github.com>
2019-03-16 10:18:20 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,16 @@
+class TestAssignment
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		for(i=0;i<4;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("\t"+"XX");
+				
+			}
+			System.out.println();
+		}
+	}
+}
