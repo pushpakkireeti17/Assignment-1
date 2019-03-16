f8128d9c7f615cdaaf741d50a07c7f36ba061015
pushpakkireeti17 <36259555+pushpakkireeti17@users.noreply.github.com>
2019-03-16 10:16:41 +0530

    Update Checkk.java

--- Checkk.java
+++ Checkk.java
@@ -1,6 +1,7 @@
 class Checkk
 {
-	int a,result=0,i;
+	int a,i;
+	final result=10;// it gives error that we cannot assign value to final variable
 	public float Average(int a)
 	{
 		for(i=1;i<=a;i++)
@@ -10,49 +11,7 @@ class Checkk
 		float avg=(result/a);
 		return avg;
 	}
- static void main(String args[])// without public it will compile but not run
-{
-	Checkk obj=new Checkk();
-	Scanner s= new Scanner(System.in);
-	System.out.println("Enter Number");
-	int a=s.nextInt();
-	System.out.println(obj.Average(a));
-}
-}
-/*class Checkk
-{
-	int a,result=0,i;
-	public float Average(int a)
-	{
-		for(i=1;i<=a;i++)
-		{
-			result =result+i;
-		}
-		float avg=(result/a);
-		return avg;
-	}
-public void main(String args[])// it says that main method should be static
-{
-	 Checkk obj=new Checkk();
-	Scanner s= new Scanner(System.in);
-	System.out.println("Enter Number");
-	int a=s.nextInt();
-	System.out.println(obj.Average(a));
-}
-}*/
-/*class Checkk
-{
-	int a,result=0,i;
-	public float Average(int a)
-	{
-		for(i=1;i<=a;i++)
-		{
-			result =result+i;
-		}
-		float avg=(result/a);
-		return avg;
-	}
-public static void main()// it says that main method not foound
+public static void main(String args[])
 {
 	Checkk obj=new Checkk();
 	Scanner s= new Scanner(System.in);
@@ -61,3 +20,48 @@ public static void main()// it says that main method not foound
 	System.out.println(obj.Average(a));
 }
 }*/
+/*class Checkk
+{
+	static int a;
+	int result=0,i;
+	public float Average(int a)
+	{
+		for(i=1;i<=a;i++)
+		{
+			result =result+i;
+		}
+		float avg=(result/a);
+		return avg;
+	}
+public static void main(String args[])
+{
+	Checkk obj=new Checkk();
+	Scanner s= new Scanner(System.in);
+	System.out.println("Enter Number");
+	int a=s.nextInt();
+	System.out.println(obj.Average(a));
+}
+}*/
+
+/*class Checkk
+{
+	int a,result=0,i;
+	public Checkk(int a)
+	{
+		this.a=a;
+	}
+	public float Average()
+	{
+		for(i=1;i<=a;i++)
+		{
+			result =result+i;
+		}
+		float avg=(result/a);
+		return avg;
+	}
+public static void main(String args[])
+{
+	Checkk obj=new Checkk();// it says that argument is required 
+	System.out.println(obj.Average());
+}
+}
